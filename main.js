a479eef40923ee7cf79fd1264d907bc41603b75e
Cyuzuzo Bienvenu <cbienvenu007@gmail.com>
2024-01-12 18:39:21 +0200

    start with 16×16 grid and remove current grid

--- main.js
+++ main.js
@@ -1,39 +1,48 @@
 const container = document.querySelector('.container');
 const choiceBtn = document.querySelector('.choice-btn');
 
+createGrid();
+
+function createGrid(num = 16) {
+	const totalSquares = num * num;
+  const squaresGrid = 500 / num; // 500(container's width) / by squares provided to get the width and height of the new Div
+
+	for (let i = 0; i < totalSquares; i++) {
+		const newDiv = document.createElement('div');
+		
+		newDiv.setAttribute('class', 'grid-element');
+
+		newDiv.style.height = `${squaresGrid}px`;
+		newDiv.style.width = `${squaresGrid}px`;
+		
+		container.appendChild(newDiv);
+
+		};
+
+		document.querySelectorAll('.grid-element').forEach((grid) => {
+			grid.addEventListener('mouseenter', function() {
+				grid.style.background = 'black';
+			})
+	})
+}
+
+function removeItem() {
+	document.querySelectorAll('.grid-element').forEach((item) => {
+		item.remove();
+	});
+}
 
 choiceBtn.addEventListener('click', function() {
+		
+	const numOfSquare = +prompt('Enter the number of squares per side?');
 
-	numOfSquares = +prompt('Enter the number of squares per side?');
-
-	if (document.querySelector('.grid-element')) {
-	 container.removeChild('grid-element');
-		// return;
-	}
-
-	if (numOfSquares > 100) {
-			alert(`You can't exceed 100 squares!`);
-			return;
+	if (numOfSquare > 64) {
+					alert(`You can't exceed 64 squares!`);
+					return;
 		}
 
-	const totalSquares = numOfSquares * numOfSquares;
-
-	const squaresGrid = 500 / numOfSquares; 
-
-
-for (let i = 0; i < totalSquares; i++) {
-	const newDiv = document.createElement('div');
+	removeItem();
 	
-	newDiv.setAttribute('class', 'grid-element');
+	createGrid(numOfSquare);
 
-	newDiv.style.height = `${squaresGrid}px`;
-	newDiv.style.width = `${squaresGrid}px`;
-	
-	container.appendChild(newDiv);
-
-	newDiv.addEventListener('mouseenter', function() {
-
-	newDiv.style.background = 'black';
 });
-
-}});
